5804a65161bcc8e8864529679d96cdcbf3ab1019
DynamiteStudio <mikailezgi25@gmail.com>
2023-10-11 09:35:35 +0200

    rin

--- P1-Javascript-/opdracht 5.1/opdracht5.1.js
+++ P1-Javascript-/opdracht 5.1/opdracht5.1.js
@@ -4,30 +4,3 @@ document.getElementById('mijnHeader').style.color = "blue";
 document.getElementById('mijnHeader').innerHTML = 'IK BEN ER!'
 
 }
-
-
-function calculate(getal1, getal2)
-{
-antwoord = getal1 + getal2;
-console.log("Hello world!");
-console.log (antwoord);
-
-}
-
-
-calculate(5,8);
-
-
-
-function Reken(getal1, getal2)
-{
-antwoord = getal1 * getal2;
-console.log("Hello world!");
-console.log (antwoord);
-
-}
-
-
-Reken(7,5);
-
-
